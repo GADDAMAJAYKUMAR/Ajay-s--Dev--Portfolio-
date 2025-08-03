f64d11251968f02020bf864b3de8d1f379d9a9a7
GADDAMAJAYKUMAR <161620177+GADDAMAJAYKUMAR@users.noreply.github.com>
2025-08-03 12:23:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./App.css";
-import GestureControl from "./components/GestureControl";
+
 
 
 function App() {
@@ -24,7 +24,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
-      <GestureControl/>
+    
     </div>
   );
 }
